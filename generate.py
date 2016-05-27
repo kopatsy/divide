e3a6091947b75936d6ee176fe6e3c2067b80dbaa
“Arthur <arthur.kopatsy@gmail.com>
2016-05-26 21:38:06 -0700

    Refactor generation

--- generate.py
+++ generate.py
@@ -7,22 +7,11 @@ import googlemaps
 from geopy.distance import distance
 from gpxpy import parser as gpxpy_parser
 
-gmap = googlemaps.Client(key='AIzaSyA-spyWeUEjIzyuW7qxiwLjmf-g60eeK2U')
-
 parser = argparse.ArgumentParser()
 parser.add_argument('gpx_filename', help='Path of the input GPX file.')
-parser.add_argument('--elevation', help='Correct elevation using Google API.', default=False)
+parser.add_argument('--html', help='Generage the HTML page.', action='store_true', default=False)
 
 args = parser.parse_args()
-
-GRADES = [
-    (-sys.maxint, 2, (0, 255, 0)),  # Green.
-    (2, 4, (255, 255, 0)),  # Yellow.
-    (4, 6, (255, 128, 0)),  # Orange.
-    (6, 8, (255, 0, 0)),  # Red.
-    (8, 10, (153, 51, 255)),  # Purple.
-    (10, sys.maxint, (0, 0, 0))  # Black.
-]
 
 with open('./services.json') as f:
     SERVICES = json.load(f)
@@ -31,102 +20,127 @@ with open(args.gpx_filename, 'r') as gpx_file:
     gpx_parser = gpxpy_parser.GPXParser(gpx_file)
     gpx_parser.parse()
 
+# Read all the data points and populates them with additional information:
+# {
+#   "distance": "Distance from start",
+#   "total_elevation": "Elevation from start",
+#   "elevation": "Elevation of that point in feet",
+#   "grade": "Grade from last point",   
+# }
 points = []
+last_point = None
+total_elevation = 0
+total_distance = 0
 for track in gpx_parser.gpx.tracks:
     for segment in track.segments:
         for point in segment.points:
-            points.append(point)
+            point_info = {
+                'grade': 0,
+                'elevation': point.elevation * 3.28084
+            }
 
-def render_from_template(directory, template_name, **kwargs):
-    loader = FileSystemLoader(directory)
-    env = Environment(loader=loader)
-    template = env.get_template(template_name)
-    return template.render(**kwargs)
+            if last_point is not None:
+                dist_diff = distance((last_point.latitude, last_point.longitude), (point.latitude, point.longitude))
+                total_distance += dist_diff.miles
+                elevation_diff = point.elevation - last_point.elevation
+                total_elevation += (elevation_diff if elevation_diff > 0 else 0)
+                point_info['grade'] = (elevation_diff / dist_diff.meters) * 100
 
-graphs = []
-cur_graph = None
+            point_info['distance'] = total_distance
+            point_info['total_elevation'] = total_elevation
 
-acc_dist = 0
-acc_elevation = 0
-cur_dist = 0
-poi_distances = {}
-for idx, point in enumerate(points):
-    if cur_graph is None:
-        cur_graph = dict(
-            from_mile=int(acc_dist / 100.0) * 100,
-            data=[],
-            pois=[],
-        )
-        cur_dist = 0
+            for poi, poi_info in SERVICES.items():
+                (dist_to_route, dist_from_start, _) = poi_info.get('location', (sys.maxint, -1, -1))
+                dist_to_point = distance((point.latitude, point.longitude), poi_info['coordinates'])
+                if dist_to_point < dist_to_route:
+                    poi_info['location'] = (dist_to_point, total_distance, total_elevation)
 
-    for poi, info in SERVICES.items():
-        d = distance((point.latitude, point.longitude), info['coordinates'])
-        if poi not in poi_distances or d < poi_distances[poi][0]:
-            poi_distances[poi] = (d, cur_graph, acc_dist, acc_elevation)
+            last_point = point
 
-    grade = 0
-    elev_diff = 0
-    if idx < len(points) - 1:
-        next_point = points[idx + 1]
-        d = distance((point.latitude, point.longitude), (next_point.latitude, next_point.longitude))
-        elev_diff = next_point.elevation - point.elevation
-        grade = (elev_diff / d.meters) * 100
+            points.append(point_info)
 
-    if idx > 0:
-        last_point = points[idx - 1]
-        d = distance((last_point.latitude, last_point.longitude), (point.latitude, point.longitude))
-        acc_dist += d.miles
-        acc_elevation += (elev_diff if elev_diff > 0 else 0)
-        cur_dist += d.miles
+dist_to_poi = {}
+for poi, poi_info in SERVICES.items():
+    (dist_to_route, dist_from_start, _) = poi_info.get('location', (sys.maxint, -1, -1))
+    if dist_to_route.miles > 1:
+        print '%s: %.1f off route' % (poi, dist_to_route.miles)
 
-    for min_grade, max_grade, color in GRADES:
-        if min_grade <= grade and grade <= max_grade:
-            break
-
-    cur_graph['data'].append(dict(
-        distance=acc_dist,
-        elevation=point.elevation * 3.28084,
-        color=color
-    ))
-
-    if cur_dist >= 100:
-        cur_graph['to_mile'] = int(acc_dist / 100.0) * 100
-        graphs.append(cur_graph)
-        cur_graph = None
-
-cur_graph['to_mile'] = int(acc_dist)
-graphs.append(cur_graph)
-
-prev_entry = None
-sorted_pois = sorted(poi_distances.items(), key=lambda e: e[1][2])  # Sorted by distance.
-for idx in range(len(sorted_pois)):
-    poi, (d, graph, distance, acc_elevation) = sorted_pois[idx]
-
-    if d > 1:
-        print '%s: %.1f off route' % (poi, d.miles)
-
-    if d > 6: 
+    if dist_to_route.miles > 6: 
         print 'IGNORED'
         continue
 
-    print poi, int(distance)
+    dist_to_poi[dist_from_start] = (poi, poi_info)
 
-    details = SERVICES[poi]
-    entry = dict(
-        name=str(poi),
-        distance=distance,
-        details=details,
-        acc_elevation=acc_elevation,
-        to_next=None,
-        to_next_elev=None
-    )
+if args.html:
+    def render_from_template(directory, template_name, **kwargs):
+        loader = FileSystemLoader(directory)
+        env = Environment(loader=loader)
+        template = env.get_template(template_name)
+        return template.render(**kwargs)
 
-    if prev_entry is not None:
-        prev_entry['to_next'] = int(distance - prev_entry['distance'])
-        prev_entry['to_next_elev'] = int((acc_elevation - prev_entry['acc_elevation']) * 3.28084)
 
-    graph['pois'].append(entry)
-    prev_entry = entry
+    GRADES = [
+        (-sys.maxint, 2, (0, 255, 0)),  # Green.
+        (2, 4, (255, 255, 0)),  # Yellow.
+        (4, 6, (255, 128, 0)),  # Orange.
+        (6, 8, (255, 0, 0)),  # Red.
+        (8, 10, (153, 51, 255)),  # Purple.
+        (10, sys.maxint, (0, 0, 0))  # Black.
+    ]
 
-with open('www/gen.html', 'w') as output:
-    output.write(render_from_template('.', 'template.js', graphs=graphs))
+    cur_graph = None
+    graphs = []
+    for point in points:
+        if cur_graph is None:
+            cur_graph = dict(
+                from_mile=int(point['distance'] / 100.0) * 100,
+                data=[],
+                pois=[],
+            )
+
+        grade = point['grade']
+        for min_grade, max_grade, color in GRADES:
+            if min_grade <= grade and grade <= max_grade:
+                break
+
+        cur_graph['data'].append(dict(
+            distance=point['distance'],
+            elevation=point['elevation'],
+            color=color
+        ))
+
+        if point['distance'] - cur_graph['from_mile'] >= 100:
+            cur_graph['to_mile'] = int(point['distance'] / 100.0) * 100
+            graphs.append(cur_graph)
+            cur_graph = None
+
+    cur_graph['to_mile'] = int(point['distance'])
+    graphs.append(cur_graph)
+
+    sorted_pois = sorted(SERVICES.items(), key=lambda e: e[1]['location'][1])  # Sorted by distance.
+    prev_entry = None
+    for poi, poi_info in sorted_pois:
+        (_, distance, acc_elevation) = poi_info['location']
+        graph = graphs[int(distance / 100)]
+
+        entry = dict(
+            name=str(poi),
+            distance=distance,
+            details=poi_info,
+            acc_elevation=acc_elevation,
+            to_next=None,
+            to_next_elev=None
+        )
+
+        if prev_entry is not None:
+            prev_entry['to_next'] = int(distance - prev_entry['distance'])
+            prev_entry['to_next_elev'] = int((acc_elevation - prev_entry['acc_elevation']) * 3.28084)
+
+        graph['pois'].append(entry)
+        prev_entry = entry
+
+    with open('www/profile.html', 'w') as output:
+        output.write(render_from_template('.', 'template.js', graphs=graphs))
+else:
+    pass
+
